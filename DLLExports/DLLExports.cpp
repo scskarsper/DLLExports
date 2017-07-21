2fea898ce67adefba46ec425a1f995f0c018d5fd
MISATOSINSAN <scskarsper@qq.com>
2017-07-22 01:37:48 +0800

    AddClassSpliter

--- DLLExports/DLLExports.cpp
+++ DLLExports/DLLExports.cpp
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <windows.h>
 #include <DbgHelp.h>
+#include <map>
+#include <string>
+using namespace std;
+typedef map<string,FILE*> fmap;
 
 bool GetDLLFileExports(TCHAR *szFileName, UINT *nNoOfExports, char **&pszFunctions, DWORD*& Addr)
 {
@@ -107,18 +111,6 @@ char* DecodeSymbolName(char* code)
 		return tmpc;
     }
 }
-TCHAR* GetLibExe()  
-{  
-   TCHAR chBuf[0x8000]={0};  
-   // 将当前路径\dll路径添加到本进程的路径中  
-   if(!GetModuleFileName(NULL,chBuf,MAX_PATH))  
-       return FALSE;  
-   *_tcsrchr(chBuf,'\\')=0;
-   _tcscat_s(chBuf,0x8000,L"\\lib.exe");
-   TCHAR* ret=(TCHAR*)malloc(sizeof(TCHAR)*_tcslen(chBuf)+1);
-   _tcscpy_s(ret,sizeof(TCHAR)*_tcslen(chBuf)+1,chBuf);
-   return ret;
-}  
 
 void Usage()
 {
@@ -135,7 +127,13 @@ void Usage()
    printf("\tDLLFile\tThe Dll file for decode into lib.\n");
    printf("The output file is dll's name with \".export.lib\" , \".export.def\" , \".export.h\" , \".export.csv\"\n");
 }
-
+BOOL contentKey(fmap Map,string Key)
+{
+	fmap::iterator l_it;
+	l_it=Map.find(Key);
+	if(l_it==Map.end()) return FALSE;
+	return TRUE;
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
 	
@@ -145,25 +143,39 @@ int _tmain(int argc, _TCHAR* argv[])
        return 0;  
    *_tcsrchr(CurrentPath,'\\')=0;
 
-	TCHAR* LibPath=GetLibExe() ;
-	if(_taccess(LibPath,0)!=0)
+	TCHAR LibPath[0x8000]={0};
+	_tcscpy_s(LibPath,0x8000,CurrentPath);
+	_tcscat_s(LibPath,0x8000,L"\\lib.exe");
+	
+	if(_taccess_s(LibPath,0)!=0)
 	{
 		printf("Cannot find the library linker:Lib.exe , please copy and leave it in the same directory of this file.\n");
 		return 0;
 	}
-	if(argc==0)
+	if(argc==1)
 	{
 		Usage();
 		return 0;
 	}
 	TCHAR* DllPath=argv[1];
+	if(DllPath[1]!=':' || _waccess(DllPath,0)!=0 || _waccess(DllPath,6)!=0)
+	{
+		TCHAR CurrentUrl[0x8000]={0}; 
+		_tcscpy_s(CurrentUrl,0x8000,CurrentPath);
+		_tcscat_s(CurrentUrl,0x8000,L"\\");
+		_tcscat_s(CurrentUrl,0x8000,argv[1]);
+		DllPath=CurrentUrl;
+	}
 	TCHAR* DllName=NULL;
 	TCHAR DllHead[MAX_PATH];
+	TCHAR DllDir[MAX_PATH]=L"";
+	TCHAR ClassHTemp[MAX_PATH]=L"";
 	TCHAR HeaderFile[MAX_PATH]=L"";
 	TCHAR LibFile[MAX_PATH]=L"";
 	TCHAR CSVFile[MAX_PATH]=L"";
 	TCHAR DefFile[MAX_PATH]=L"";
 	TCHAR CFile[MAX_PATH]=L"";
+	fmap ClassHeaders;
 	if(_waccess(DllPath,0)!=0 || _waccess(DllPath,6)!=0)
 	{
 		printf("Cannot open the file %S.\n",DllPath);
@@ -173,16 +185,29 @@ int _tmain(int argc, _TCHAR* argv[])
    *_tcsrchr(DllHead,'.')=0;
    DllName=_tcsrchr(DllHead,'\\');
    if(DllName==NULL)DllName=DllPath;else DllName++;
+    _tcscat_s(DllDir,MAX_PATH,DllHead);
+    _tcscat_s(DllDir,MAX_PATH,L".exports");
+   _tmkdir(DllDir);
     _tcscat_s(HeaderFile,MAX_PATH,DllHead);
-    _tcscat_s(HeaderFile,MAX_PATH,L".exports.h");
+    _tcscat_s(HeaderFile,MAX_PATH,L".exports\\");
+	_tcscat_s(HeaderFile,MAX_PATH,DllName);
+    _tcscat_s(HeaderFile,MAX_PATH,L".h");
     _tcscat_s(LibFile,MAX_PATH,DllHead);
-    _tcscat_s(LibFile,MAX_PATH,L".exports.lib");
+    _tcscat_s(LibFile,MAX_PATH,L".exports\\");
+	_tcscat_s(LibFile,MAX_PATH,DllName);
+    _tcscat_s(LibFile,MAX_PATH,L".lib");
     _tcscat_s(CSVFile,MAX_PATH,DllHead);
-    _tcscat_s(CSVFile,MAX_PATH,L".exports.csv");
+    _tcscat_s(CSVFile,MAX_PATH,L".exports\\");
+	_tcscat_s(CSVFile,MAX_PATH,DllName);
+    _tcscat_s(CSVFile,MAX_PATH,L".csv");
     _tcscat_s(DefFile,MAX_PATH,DllHead);
-    _tcscat_s(DefFile,MAX_PATH,L".exports.def");
+    _tcscat_s(DefFile,MAX_PATH,L".exports\\");
+	_tcscat_s(DefFile,MAX_PATH,DllName);
+    _tcscat_s(DefFile,MAX_PATH,L".def");
     _tcscat_s(CFile,MAX_PATH,DllHead);
-    _tcscat_s(CFile,MAX_PATH,L".exports.c");
+    _tcscat_s(CFile,MAX_PATH,L".exports\\");
+	_tcscat_s(CFile,MAX_PATH,DllName);
+    _tcscat_s(CFile,MAX_PATH,L".c");
 
     printf("Balthasar Toolboxes Version 1.0.0.0\n");
     printf("Copyright (C) RCAWorks Studio(Balthasar). All rights reserved.\n\n");
@@ -190,7 +215,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	printf("\tDllFile: %S\n",DllPath);
 	printf("Output:\n");
 	printf("\tHeader:  %S\n",HeaderFile);
-	printf("\tCFile:  %S\n",CFile);
+	printf("\tCFile:   %S\n",CFile);
 	printf("\tLibFile: %S\n",LibFile);
 	printf("\tCSVMap:  %S\n",CSVFile);
 	printf("\tDefine:  %S\n",DefFile);
@@ -219,16 +244,14 @@ int _tmain(int argc, _TCHAR* argv[])
 			haveNoClass=TRUE;
 		}
 	}
-	fprintf_s(hFile,"#pragma once\n#include <windows.h>\n");
 	fprintf_s(cFile,"#include \"%S.exports.h\"\n",DllName);
 	if(haveNoClass)
 	{
+			printf("\nAnalysis:  Find Cdecl Exports,Creating Dynamic Mapper in HeaderFile:%S.h\n",DllName);
+			printf("Analysis:  Find Cdecl Exports,Creating Dynamic Code in CppFile:%S.c\n",DllName);
 			fprintf_s(hFile,"class %S\n{\nprivate:\n\tHMODULE handle;\npublic:\n\t%S(void);\n\t~%S(void);\n",DllName,DllName,DllName);
 			fprintf_s(cFile,"%S::~%S(void)\n{\n\tif(handle!=NULL)FreeLibrary(handle);\n}\n\n",DllName,DllName);
 			fprintf_s(cFile,"%S::%S(void)\n{\n\thandle=LoadLibrary(_T(\"%S.dll\"));\n",DllName,DllName,DllName);
-	}else
-	{
-			fprintf_s(hFile,"#pragma comment(lib,\"%S.exports.lib\")\n",DllName);
 	}
     for(UINT i=0; i<unNoOfExports; i++)
 	{
@@ -240,7 +263,44 @@ int _tmain(int argc, _TCHAR* argv[])
 		if(Symbol[0]=='?')
 		{
 			//isClass
-			fprintf_s(hFile,"%s\n",DeSymbol);
+			char Tmp[0x8000]="";
+			strcpy_s(Tmp,0x8000,DeSymbol);
+			char* kh=strchr(Tmp,'(');
+			if(kh!=NULL)*kh=0;
+			char* TmpRev=_strrev(Tmp);
+			char* classptr=strstr(TmpRev,"::");
+			char* classname;
+			if(classptr==NULL)
+			{
+				classname="default";
+			}else
+			{
+				char* RRev2=classptr+2;
+				RRev2=_strrev(RRev2);
+				classname=strrchr(Tmp,' ');
+				if(classname==NULL)
+				{
+					classname="default";
+				}else
+				{
+					classname++;
+				}
+			}
+			char* classptr2=strstr(classname,"::");
+			if(classptr2!=NULL)*classptr2=0;
+			string classKey=classname;
+			if(!contentKey(ClassHeaders,classKey))
+			{
+				printf("Analysis:  Find Class Exports,Creating HeaderFile: ClassHeaders\\%S.%s.h\n",DllName,classKey.c_str());
+				FILE* TFile;
+				wsprintf(ClassHTemp,L"%s\\ClassHeaders",DllDir);
+				_tmkdir(ClassHTemp);
+				wsprintf(ClassHTemp,L"%s\\ClassHeaders\\%s.%S.h",DllDir,DllName,classname);
+				_wfopen_s(&TFile,ClassHTemp,L"w");
+				ClassHeaders.insert(pair<string,FILE*>(classKey,TFile));
+			}
+			FILE* TFile=ClassHeaders[classKey];
+			fprintf_s(TFile,"%s\n",DeSymbol);
 		}else
 		{
 			fprintf_s(hFile,"\tPVOID %s;\n",Symbol);
@@ -253,6 +313,36 @@ int _tmain(int argc, _TCHAR* argv[])
 		fprintf_s(hFile,"};\n");
 		fprintf_s(cFile,"}\n");
 	}
+	fclose(hFile);
+	char buf[0x8000];
+	_wfopen_s(&hFile,HeaderFile,L"r");
+	fseek(hFile,0,SEEK_END);
+	long size=ftell(hFile);
+	char* btmp=(char*)malloc(sizeof(char)*size);
+	memset(btmp,0,sizeof(char)*size);
+	fseek(hFile,0,SEEK_SET);
+	//fread_s(btmp,sizeof(char)*size,sizeof(char),size,hFile);
+	while(fgets(buf,0x8000,hFile))
+	{
+		strcat_s(btmp,sizeof(char)*size,buf);
+	};
+	fclose(hFile);
+	_wfopen_s(&hFile,HeaderFile,L"w");
+	fseek(hFile,0,SEEK_SET);
+	fprintf_s(hFile,"#pragma once\n#include <windows.h>\n");
+	fmap::iterator iter;
+	if(!ClassHeaders.empty())
+	{
+		fprintf_s(hFile,"#pragma comment(lib,\"%S.exports.lib\")\n",DllName);
+	}
+	for (iter=ClassHeaders.begin();iter!=ClassHeaders.end();++iter)
+	{
+		fprintf_s(hFile,"#include \"ClassHeaders\\%S.%s.h\"\n",DllName,iter->first.c_str());
+		fclose(iter->second);
+	}
+	
+	if(size>0)fprintf_s(hFile,"%s",btmp);
+	free(btmp);
 	fclose(hFile);
 	fclose(csvFile);
 	fclose(defFile);
