0c1b113ad4e723ec9d7bc1e6ad26e9c3328afaac
MISATOSINSAN <scskarsper@qq.com>
2017-07-24 16:12:02 +0800

    Add Class Anasyser

--- DLLExports/DLLExports.cpp
+++ DLLExports/DLLExports.cpp
@@ -7,10 +7,18 @@
 #include <windows.h>
 #include <DbgHelp.h>
 #include <map>
+#include <list>
 #include <string>
+
 using namespace std;
 typedef map<string,FILE*> fmap;
+typedef list<string> flines;
+typedef map<string,flines> fhrebs;
 
+BOOL ShowDebug=FALSE;
+BOOL DoClassFormat=TRUE;
+
+void ClassHeaderFormater(const char* className,TCHAR* ClassTPath);
 bool GetDLLFileExports(TCHAR *szFileName, UINT *nNoOfExports, char **&pszFunctions, DWORD*& Addr)
 {
     HANDLE hFile;
@@ -123,13 +131,23 @@ void Usage()
    EFile++;
    printf("Balthasar Toolboxes Version 1.0.0.0\n");
    printf("Copyright (C) RCAWorks Studio(Balthasar). All rights reserved.\n\n");
-   printf("Usage: %S [DLLFile]\n",EFile);
+   printf("Usage: %S [DLLFile] [OPTIONS]\n",EFile);
    printf("\tDLLFile\tThe Dll file for decode into lib.\n");
+   printf("OPTIONS:\n");
+   printf("\t-d\tShow Debug Messages.\n");
+   printf("\t-O\tDo not format class header to normal header.\n");
    printf("The output file is dll's name with \".export.lib\" , \".export.def\" , \".export.h\" , \".export.csv\"\n");
 }
 BOOL contentKey(fmap Map,string Key)
 {
 	fmap::iterator l_it;
+	l_it=Map.find(Key);
+	if(l_it==Map.end()) return FALSE;
+	return TRUE;
+}
+BOOL IsHrebsKey(fhrebs Map,string Key)
+{
+	fhrebs::iterator l_it;
 	l_it=Map.find(Key);
 	if(l_it==Map.end()) return FALSE;
 	return TRUE;
@@ -166,6 +184,21 @@ int _tmain(int argc, _TCHAR* argv[])
 		_tcscat_s(CurrentUrl,0x8000,argv[1]);
 		DllPath=CurrentUrl;
 	}
+
+	for(int i=2;i<argc;i++)
+	{
+		TCHAR* OAC=argv[i];
+		wstring OA=OAC;
+		if(OA==L"-d")
+		{
+			ShowDebug=TRUE;
+		}
+		if(OA==L"-O")
+		{
+			DoClassFormat=FALSE;
+		}
+	}
+
 	TCHAR* DllName=NULL;
 	TCHAR DllHead[MAX_PATH];
 	TCHAR DllDir[MAX_PATH]=L"";
@@ -232,7 +265,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	_wfopen_s(&csvFile,CSVFile,L"w");
 	_wfopen_s(&defFile,DefFile,L"w");
     GetDLLFileExports(DllPath, &unNoOfExports, lppBuffer,lppAddr);
-	
+	if(lppBuffer==NULL)
+	{
+		printf("Cannot Open File!\n");
+		return 0;
+	}
 	fprintf_s(csvFile,"\"%s\",\"%s\",\"%s\",\"%s\"\n","HINT","RVAddress","FunctionDeclare","FunExportSymbol");
     fprintf_s(defFile,"LIBRARY %S \nEXPORTS\n",DllName);
 	BOOL haveNoClass=FALSE;
@@ -265,9 +302,25 @@ int _tmain(int argc, _TCHAR* argv[])
 			//isClass
 			char Tmp[0x8000]="";
 			strcpy_s(Tmp,0x8000,DeSymbol);
-			char* kh=strchr(Tmp,'(');
+			
+			char* kh=strchr(Tmp,'{');
 			if(kh!=NULL)*kh=0;
-			char* TmpRev=_strrev(Tmp);
+			
+			kh=strchr(Tmp,'(');
+			if(kh!=NULL)*kh=0;
+			
+			char* k2=strrchr(Tmp,' ');
+			if(k2!=NULL)k2++;
+			
+			while(strstr(k2,"::")==NULL && strstr(Tmp,"::")!=NULL)
+			{
+				if(k2!=NULL)k2--;
+				*k2=0;
+				k2=strrchr(Tmp,' ');
+				if(k2!=NULL)k2++;
+			}
+
+			char* TmpRev=_strrev(k2);
 			char* classptr=strstr(TmpRev,"::");
 			char* classname;
 			if(classptr==NULL)
@@ -277,14 +330,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			{
 				char* RRev2=classptr+2;
 				RRev2=_strrev(RRev2);
-				classname=strrchr(Tmp,' ');
-				if(classname==NULL)
-				{
-					classname="default";
-				}else
-				{
-					classname++;
-				}
+				classname=RRev2;
 			}
 			char* classptr2=strstr(classname,"::");
 			if(classptr2!=NULL)*classptr2=0;
@@ -314,6 +360,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		fprintf_s(cFile,"}\n");
 	}
 	fclose(hFile);
+
 	char buf[0x8000];
 	_wfopen_s(&hFile,HeaderFile,L"r");
 	fseek(hFile,0,SEEK_END);
@@ -330,15 +377,22 @@ int _tmain(int argc, _TCHAR* argv[])
 	_wfopen_s(&hFile,HeaderFile,L"w");
 	fseek(hFile,0,SEEK_SET);
 	fprintf_s(hFile,"#pragma once\n#include <windows.h>\n");
+	//格式化Class
 	fmap::iterator iter;
 	if(!ClassHeaders.empty())
 	{
-		fprintf_s(hFile,"#pragma comment(lib,\"%S.exports.lib\")\n",DllName);
+		fprintf_s(hFile,"#pragma comment(lib,\"%S.lib\")\n",DllName);
 	}
-	for (iter=ClassHeaders.begin();iter!=ClassHeaders.end();++iter)
+	if(DoClassFormat)
 	{
-		fprintf_s(hFile,"#include \"ClassHeaders\\%S.%s.h\"\n",DllName,iter->first.c_str());
-		fclose(iter->second);
+		for (iter=ClassHeaders.begin();iter!=ClassHeaders.end();++iter)
+		{
+			fprintf_s(hFile,"#include \"ClassHeaders\\%S.%s.h\"\n",DllName,iter->first.c_str());
+			fclose(iter->second);
+		
+			wsprintf(ClassHTemp,L"%s\\ClassHeaders\\%s.%S.h",DllDir,DllName,iter->first.c_str());
+			ClassHeaderFormater(iter->first.c_str(),ClassHTemp);
+		}
 	}
 	
 	if(size>0)fprintf_s(hFile,"%s",btmp);
@@ -350,4 +404,215 @@ int _tmain(int argc, _TCHAR* argv[])
 	wsprintf(CMD,L"/def:\"%s\" /machine:%s /out:\"%s\"",DefFile,L"i386",LibFile);
 	ShellExecute(0,L"open",LibPath,CMD,CurrentPath,0);
 	return 0;
+}
+
+
+BOOL contentLine(flines lines,string Key)
+{
+	flines::iterator liter;
+	for (liter=lines.begin();liter!=lines.end();++liter)
+	{
+		if(*liter==Key)
+		{
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+void ReplaceClass(flines &addonsHeader,string &line,string Header,string Org,string Dst)
+{
+	int wx=line.find(Org);
+	if(wx==-1)return;
+	while(wx!=-1)
+	{
+		int wa=Org.length();
+		line=line.substr(0,wx)+Dst+line.substr(wx+wa);
+		wx=line.find(Org);
+	}
+	if(Header=="")return;
+	string ok="#include <"+Header+">";
+	if(!contentLine(addonsHeader,ok))
+	{
+		addonsHeader.push_back(ok);
+	}
+}
+void FindClass(flines &addonsHeader,string &line,string Header,string Org)
+{
+	if(Header=="")return;
+	int wx=line.find(Org);
+	if(wx==-1)return;
+	string ok="#include <"+Header+">";
+	if(!contentLine(addonsHeader,ok))
+	{
+		addonsHeader.push_back(ok);
+	}
+}
+void ConvertGlass(flines &addonsHeader,string &line)
+{                                                         
+	ReplaceClass(addonsHeader,line,"atlstr.h","class ATL::CStringT<wchar_t,class StrTraitMFC_DLL<wchar_t,class ATL::ChTraitsCRT<wchar_t> > >","CAtlStringW");
+	ReplaceClass(addonsHeader,line,"atlstr.h","class ATL::CStringT<wchar_t,class StrTraitMFC_DLL<wchar_t> >","CAtlStringW");
+	ReplaceClass(addonsHeader,line,"atlstr.h","class ATL::CStringT<char,class StrTraitMFC_DLL<char,class ATL::ChTraitsCRT<char> > >","CAtlStringA");
+	ReplaceClass(addonsHeader,line,"atlstr.h","class ATL::CStringT<char,class StrTraitMFC_DLL<char> >","CAtlStringA");
+	FindClass(addonsHeader,line,"afxtempl.h","class class CList");
+	FindClass(addonsHeader,line,"vector","class std::vector");
+	FindClass(addonsHeader,line,"allocators","class std::allocator");
+	FindClass(addonsHeader,line,"deque","class std::deque");
+	FindClass(addonsHeader,line,"list","class std::list");
+	FindClass(addonsHeader,line,"map","class std::map");
+	FindClass(addonsHeader,line,"sort","class std::sort");
+	FindClass(addonsHeader,line,"hash_map","class std::hash_map");
+	FindClass(addonsHeader,line,"hash_set","class std::hash_set");
+}
+
+void ClassHeaderFormater(const char* className,TCHAR* ClassTPath)
+{
+	BOOL isDefStruct=FALSE;
+	if(strcmp(className,"default")==0)
+	{
+		isDefStruct=TRUE;
+	}
+	TCHAR NewPath[MAX_PATH]=_T("");
+	if(_waccess_s(ClassTPath,6)==0)
+	{
+		wcscpy_s(NewPath,MAX_PATH,ClassTPath);
+		TCHAR* dot=wcsrchr(NewPath,'.');
+		if(dot!=NULL)*dot=0;
+		wcscat_s(NewPath,MAX_PATH,_T(".hdef"));
+		CopyFile(ClassTPath,NewPath,FALSE);
+		printf("Format:  Class HeaderFile: %S\n",ClassTPath);
+	}else
+	{
+		printf("Error:  Format Class HeaderFile: %S\n",ClassTPath);
+		return;
+	}
+	if(_waccess_s(NewPath,6)!=0)
+	{
+		printf("Error:  Load Class DefHeaderFile: %S\n",NewPath);
+		return;
+	}
+	FILE* hDef;
+	_wfopen_s(&hDef,NewPath,L"r");
+	if(hDef==NULL)
+	{
+		printf("Error:  Open Class DefHeaderFile: %S\n",NewPath);
+		return;
+	}
+	FILE* hFile;
+	_wfopen_s(&hFile,ClassTPath,L"w");
+	if(hFile==NULL)
+	{
+		printf("Error:  Create Class HeaderFile: %S\n",ClassTPath);
+		return;
+	}
+	flines lines;
+	flines addonsHeader;
+	fhrebs lhrebs;
+	char linebuf[0x8000];
+	while(fgets(linebuf,0x8000,hDef))
+	{
+		string s=linebuf;
+		lines.push_back(s);
+	}
+	fclose(hDef);
+	//lines.sort();
+	
+	if(isDefStruct)
+	{
+		fprintf_s(hFile,"#pragma once\n");
+		flines::iterator liter;
+		for (liter=lines.begin();liter!=lines.end();++liter)
+		{
+			if(ShowDebug)printf("Work for data:%s\n",(*liter).c_str());
+			string line=*liter;
+			char lchar=line.c_str()[line.length()-1];
+			if(lchar=='\n')
+			{
+				line=line.substr(0,line.length()-1)+";\n";
+			}
+			fprintf_s(hFile,"%s",line.c_str());
+		}
+	}
+	else
+	{
+		flines::iterator liter;
+		for (liter=lines.begin();liter!=lines.end();++liter)
+		{
+			if(ShowDebug)printf("Work for data:%s\n",(*liter).c_str());
+			string line=*liter;
+			int x=line.find(':');
+			string inhrebs=line.substr(0,x);
+			if(inhrebs=="protected" || inhrebs=="public" || inhrebs=="private")
+			{
+				line=line.substr(inhrebs.length()+1);
+				if(line.substr(0,1)==" ")
+				{
+					line=line.substr(1);
+				}
+			}else
+			{
+				inhrebs="default";
+			}
+			//删除thiscall
+			int w=line.find("__thiscall ");//len=11
+			if(w>-1)
+			{
+				line=line.substr(0,w)+line.substr(w+11);
+			}
+			//删除类名
+			string oc=className;
+			oc=oc+"::";
+			w=line.find(oc);
+			if(w>-1)
+			{
+				line=line.substr(0,w)+line.substr(w+oc.length());
+			}
+			//删除无效修饰
+			if(line.find("const `vftable'")!=-1)continue;
+			if(line.find("default constructor closure")!=-1)continue;
+			char tap[0x8000]="";
+			sprintf_s(tap,0x8000,"operator=(class %s const",className);
+			if(line.find(tap)!=-1)continue;
+			//添加结束符
+			char lchar=line.c_str()[line.length()-1];
+			if(lchar=='\n')
+			{
+				line=line.substr(0,line.length()-1)+";\n";
+			}
+			ConvertGlass(addonsHeader,line);
+
+			if(!IsHrebsKey(lhrebs,inhrebs))
+			{
+				flines tlines;
+				lhrebs.insert(pair<string,flines>(inhrebs,tlines));
+			}
+			lhrebs[inhrebs].push_back(line);
+		}
+	
+		fprintf_s(hFile,"#pragma once\n");
+		for (liter=addonsHeader.begin();liter!=addonsHeader.end();++liter)
+		{
+			fprintf_s(hFile,"%s\n",(*liter).c_str());
+		}
+		fprintf_s(hFile,"class __declspec(dllimport) %s\n{\n",className);
+		if(IsHrebsKey(lhrebs,"default"))
+		{
+			for (liter=lhrebs["default"].begin();liter!=lhrebs["default"].end();++liter)
+			{
+				fprintf_s(hFile,"\t%s",(*liter).c_str());
+			}
+		}
+		fhrebs::iterator hiter;
+		for (hiter=lhrebs.begin();hiter!=lhrebs.end();++hiter)
+		{
+			if(hiter->first=="default")continue;
+			fprintf_s(hFile,"%s:\n",hiter->first.c_str());
+			for (liter=hiter->second.begin();liter!=hiter->second.end();++liter)
+			{
+				if(ShowDebug)printf("Write for data:%s\n",(*liter).c_str());
+				fprintf_s(hFile,"\t%s",(*liter).c_str());
+			}
+		}
+		fprintf_s(hFile,"};\n",className);
+	}
+	fclose(hFile);
 }
